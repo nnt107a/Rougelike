005e0a660aba2ff7814b1a5b29373e80ba99089d
Nguyễn Nhật Tân <22120325@student.hcmus.edu.vn>
2025-03-06 22:25:19 +0700

    modify enemy collision with player, add player's hurt animation

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/Enemy/Skeleton.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/Enemy/Skeleton.cs
@@ -11,8 +11,8 @@ public class Skeleton : Enemy
         base.Awake();
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
-        capsuleCollider = GetComponent<CapsuleCollider2D>();
-        circleCollider = GetComponent<CircleCollider2D>();
+        capsuleCollider = GetComponentInChildren<CapsuleCollider2D>();
+        circleCollider = GetComponentInChildren<CircleCollider2D>();
         navMeshAgent = GetComponent<NavMeshAgent>();
 
         navMeshAgent.updateRotation = false;
@@ -132,6 +132,7 @@ public class Skeleton : Enemy
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<PlayerAttack>().TakeDamage(attackDamage);
+            collision.GetComponent<PlayerAttack>().ActivateAnim();
         }
     }
     public void Deactivate()
--- Assets/Cainos/Pixel Art Top Down - Basic/Script/Player/PlayerAttack.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/Player/PlayerAttack.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerAttack : Health
 {
@@ -7,12 +8,46 @@ public class PlayerAttack : Health
 
     [Header("Player resources")]
     [SerializeField] public Mana mana;
+
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
     
     private void Awake()
     {
         base.Awake();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void ActivateAnim()
+    {
+        if (currentHealth > 0)
+        {
+            anim.Play("hit");
+            StartCoroutine(IFrame());
+        }
+        else
+        {
+            anim.Play("die");
+        }
+    }
+    private IEnumerator IFrame()
+    {
+        Physics2D.IgnoreLayerCollision(23, 8, true);
+        Physics2D.IgnoreLayerCollision(23, 9, true);
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g,
+                                         spriteRenderer.color.b, 0.2f);
+        yield return new WaitForSeconds(28f / 60);
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g,
+                                         spriteRenderer.color.b, 1);
+        Physics2D.IgnoreLayerCollision(23, 8, false);
+        Physics2D.IgnoreLayerCollision(23, 9, false);
     }
     private void Update()
     {
+    }
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 0;
     }
 }
